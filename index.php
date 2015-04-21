e1c06ba26bade146568b2f823e600560f72f2953
RomainChr <romain.chery@gmail.com>
2015-04-21 15:01:01 +0200

    test

--- index.php
+++ index.php
@@ -90,15 +90,17 @@
                 
                 echo "----------------";
                 
+                
+                
                 try {
 
                     // Upload to a user's profile. The photo will be in the
                     // first album in the profile. You can also upload to
                     // a specific album by using /ALBUM_ID as the path     
                     $response = (new FacebookRequest(
-                    $session, 'POST', '/me/photos', array(
-                    'source' => new CURLFile('path/to/file.name', 'image/png'),
-                    'message' => 'User provided message'
+                        $session, 'POST', '/me/photos', array(
+                        'source' => new CURLFile('@/path/to/file.name', 'image/png'),
+                        'message' => 'User provided message'
                     )
                     ))->execute()->getGraphObject();
 
