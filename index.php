3ebe150386d6ffab9ee3664edf8eec745683a1a7
RomainChr <romain.chery@gmail.com>
2015-04-21 15:45:26 +0200

    test avec try

--- index.php
+++ index.php
@@ -92,29 +92,29 @@
                 
                 
                 
-//                try {
-//
-//                    // Upload to a user's profile. The photo will be in the
-//                    // first album in the profile. You can also upload to
-//                    // a specific album by using /ALBUM_ID as the path     
-//                    $response = (new FacebookRequest(
-//                    $session, 'POST', '/me/photos', array(
-//                        'source' => new CURLFile('path/to/file.name', 'image/png'),
-//                        'message' => 'User provided message'
-//                    )
-//                    ))->execute()->getGraphObject();
-//
-//                    // If you're not using PHP 5.5 or later, change the file reference to:
-//                    // 'source' => '@/path/to/file.name'
-//
-//                    echo "Posted with id: " . $response->getProperty('id');
-//                    echo $facebook
-//                } catch(FacebookRequestException $e) {
-//
-//                    echo "Exception occured, code: " . $e->getCode();
-//                    echo " with message: " . $e->getMessage();
-//
-//                } 
+                try {
+
+                    // Upload to a user's profile. The photo will be in the
+                    // first album in the profile. You can also upload to
+                    // a specific album by using /ALBUM_ID as the path     
+                    $response = (new FacebookRequest(
+                        $session, 'POST', '/me/photos', array(
+                            'source' => new CURLFile('path/to/file.name', 'image/png'),
+                            'message' => 'User provided message'
+                        )
+                    ))->execute()->getGraphObject();
+
+                    // If you're not using PHP 5.5 or later, change the file reference to:
+                    // 'source' => '@/path/to/file.name'
+
+                    echo "Posted with id: " . $response->getProperty('id');
+                    echo $facebook;
+                } catch(FacebookRequestException $e) {
+
+                    echo "Exception occured, code: " . $e->getCode();
+                    echo " with message: " . $e->getMessage();
+
+                } 
                 
 //                $response = (new FacebookRequest(
 //                  $session, 'POST', '/me/photos', array(
